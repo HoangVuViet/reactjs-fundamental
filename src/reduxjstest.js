f2c4c888d6069eec61c442dd5f1ab51585e731da
mikhail-kalashnikov <hoangk62uet@gmail.com>
2020-07-13 17:35:45 +0700

    asdasd

--- src/reduxjstest.js
+++ src/reduxjstest.js
@@ -1,14 +1,51 @@
-import { createStore } from 'redux';
-
-let initialState = {
-  title: 'Kind of Blue',
-  artist: 'Miles Davis',
-  year: 1959,
+// Actions
+const ADD_USER = (payload) => {
+  return { type: 'ADD_USER', payload: payload };
 };
-function reducer(state, action) {
-  return state;
-}
-let store = createStore(reducer, initialState);
-let state = store.getState();
-console.log(state);
-sadasdasdasdasd;
+const UPDATE_USER = (payload) => {
+  return { type: 'UPDATE_USER', payload: payload };
+};
+//Reducers
+let id = 1;
+const user = (state, action) => {
+  switch (action.type) {
+    case 'ADD_USER':
+      return [
+        ...state,
+        {
+          id: id++,
+          name: action.payload.name,
+          age: action.payload.age,
+          job: action.payload.job,
+        },
+      ];
+    case 'UPDATE_USER':
+      return state.map((user) => {
+        if (user.id !== action.payload.id) {
+          return user;
+        } else {
+          return {
+            ...user,
+            name: action.payload.name || user.name,
+            age: action.payload.age || user.age,
+            job: action.payload.job || user.job,
+          };
+        }
+      });
+    default:
+      return state;
+  }
+};
+//Store
+let { createStore, applyMiddleware } = Redux;
+const store = createStore(user, []);
+console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+});
+store.dispatch(ADD_USER({ name: 'Hoang Vu', age: 20, job: 'Sudent' }));
+store.dispatch(ADD_USER({ name: 'Hoang', age: 2, job: 'Stuent' }));
+store.dispatch(ADD_USER({ name: 'Vu', age: 0, job: 'Studet' }));
+store.dispatch(
+  UPDATE_USER({ id: 2, name: 'Vu Viet', age: 24, job: 'Student' })
+);
