09de95fed86aa2ab2a5b85f85e5636b6dade035a
mikhail-kalashnikov <hoangk62uet@gmail.com>
2020-07-29 08:45:04 +0700

    abc

---
+++ routing--01-start/src/containers/Blog/Blog.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+// import axios from 'axios';
+import { Route, Link } from 'react-router-dom';
+import './Blog.css';
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+class Blog extends Component {
+  render() {
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link
+                  to={{
+                    pathname: '/new-post',
+                    hash: '#submit',
+                    search: '?quick-submit=true ',
+                  }}
+                >
+                  New Post
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        {/* <Posts></Posts> */}
+        {/* <Route path="/" exact render={() => <h1>Home</h1>}></Route>
+        <Route path="/" render={() => <h1>Home</h1>}></Route> */}
+        <Route path="/" exact component={Posts}></Route>
+        <Route path="/new-post" exact component={NewPost}></Route>
+      </div>
+    );
+  }
+}
+
+export default Blog;
