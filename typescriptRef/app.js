5138eaa7dd21a21d622debae440e045074a8e1d4
mikhail-kalashnikov <hoangk62uet@gmail.com>
2020-07-30 11:36:52 +0700

    object ts

--- typescriptRef/app.js
+++ typescriptRef/app.js
@@ -1,10 +1,28 @@
-function add(num1, num2) {
-  if (typeof num1 !== 'number' && typeof num2 !== 'number') {
-    throw new Error('Incorrect Input');
-  }
-  return num1 + num2;
+function add(num1, num2, showResult, resultPhrase) {
+    // if (typeof num1 !== 'number' && typeof num2 !== 'number') {
+    //   throw new Error('Incorrect Input');
+    // }
+    const result = num1 + num2;
+    if (showResult) {
+        console.log(resultPhrase + result);
+    }
+    else {
+        return num1 + num2;
+    }
 }
-var num1 = 5;
-var num2 = 3.2;
-var result = add(num1, num2);
+let num1;
+num1 = 5;
+const num2 = 3.2;
+const printResult = true;
+const resultPhrase = 'Result is:';
+const result = add(num1, num2, printResult, resultPhrase);
 console.log(result);
+//Object types
+const person = {
+    name: 'Hoang',
+    age: 21,
+    hobbies: ['Sports', 'Coding'],
+    role: [2, 'Dev'],
+};
+person.role.push('Gamer');
+console.log(person);
--- typescriptRef/app.tsx
+++ typescriptRef/app.tsx
@@ -1,15 +1,37 @@
-function add(num1: number, num2: number, showResult: boolean) {
+function add(
+  num1: number,
+  num2: number,
+  showResult: boolean,
+  resultPhrase: string
+) {
   // if (typeof num1 !== 'number' && typeof num2 !== 'number') {
   //   throw new Error('Incorrect Input');
   // }
-  if (showResult === true) {
-    console.log(num1 + num2);
+  const result = num1 + num2;
+  if (showResult) {
+    console.log(resultPhrase + result);
   } else {
     return num1 + num2;
   }
 }
-const num1 = 5;
+let num1: number;
+num1 = 5;
 const num2 = 3.2;
 const printResult = true;
-const result = add(num1, num2, printResult);
+const resultPhrase = 'Result is:';
+const result = add(num1, num2, printResult, resultPhrase);
 console.log(result);
+//Object types
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  name: 'Hoang',
+  age: 21,
+  hobbies: ['Sports', 'Coding'],
+  role: [2, 'Dev'],
+};
+person.role.push('Gamer');
+console.log(person);
